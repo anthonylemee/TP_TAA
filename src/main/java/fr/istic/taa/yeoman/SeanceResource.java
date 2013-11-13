d400f54e6c7c203aa2896f0b6addfdf7963ad9b5
Anthony LE MEE <anthonylemee@gmail.com>
2013-11-13 11:18:06 +0100

    Modifications sur le path de la ressource de Seance

--- src/main/java/fr/istic/taa/yeoman/SeanceResource.java
+++ src/main/java/fr/istic/taa/yeoman/SeanceResource.java
@@ -13,7 +13,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/sceance")
+@Path("/seances")
 public class SeanceResource {
 
     private List<Seance> seances = new ArrayList<Seance>();
