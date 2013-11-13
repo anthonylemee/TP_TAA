8e05230cfa0079d74bb1338aebc326c3ff855793
Anthony LE MEE <anthonylemee@gmail.com>
2013-11-13 11:05:43 +0100

    ajout de la classe Sport

---
+++ src/main/java/fr/istic/taa/yeoman/Sport.java
@@ -0,0 +1,228 @@
+package fr.istic.taa.yeoman;
+import java.util.Set;
+import java.util.HashSet;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+ 
+@javax.persistence.Entity 
+public class Sport
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.Id 
+	@javax.persistence.Column(nullable = false) 
+	protected Long id;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.Column(nullable = false) 
+	protected String nom_sport;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.Column(nullable = false) 
+	protected String type_sport;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	 
+	@javax.persistence.OneToMany 
+	protected Set<Seance> seance;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Sport(){
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public long getId() {
+		return this.id;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getNom_sport() {
+		return this.nom_sport;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public String getType_sport() {
+		return this.type_sport;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public Set<Seance> getSeance() {
+		if(this.seance == null) {
+				this.seance = new HashSet<Seance>();
+		}
+		return (Set<Seance>) this.seance;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addAllSeance(Set<Seance> newSeance) {
+		if (this.seance == null) {
+			this.seance = new HashSet<Seance>();
+		}
+		for (Seance tmp : newSeance)
+			tmp.setSport(this);
+			
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeAllSeance(Set<Seance> newSeance) {
+		if(this.seance == null) {
+			return;
+		}
+		
+		this.seance.removeAll(newSeance);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setId(long myId) {
+		this.id = myId;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setNom_sport(String myNom_sport) {
+		this.nom_sport = myNom_sport;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void setType_sport(String myType_sport) {
+		this.type_sport = myType_sport;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void addSeance(Seance newSeance) {
+		if(this.seance == null) {
+			this.seance = new HashSet<Seance>();
+		}
+		
+		if (this.seance.add(newSeance))
+			newSeance.basicSetSport(this);	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetId() {
+		this.id = 0L;	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetNom_sport() {
+		this.nom_sport = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void unsetType_sport() {
+		this.type_sport = "";	
+	}
+	
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 * @ordered
+	 */
+	public void removeSeance(Seance oldSeance) {
+		if(this.seance == null)
+			return;
+		
+		if (this.seance.remove(oldSeance))
+			oldSeance.unsetSport();
+			
+	}
+	
+}
+
