645ea0168481aa2053fa8bca07b35a6cf4fb6c5c
jimmy dano <dano.jimmy@gmail.com>
2013-11-21 11:11:13 +0100

    no message

--- src/main/java/fr/istic/taa/yeoman/entityRessources/SeanceResource.java
+++ src/main/java/fr/istic/taa/yeoman/entityRessources/SeanceResource.java
@@ -1,56 +1,145 @@
 
 package fr.istic.taa.yeoman.entityRessources;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
 
+import fr.istic.taa.yeoman.dao.DaoSeance;
 import fr.istic.taa.yeoman.entity.Seance;
 
 @Path("/seances")
 public class SeanceResource {
 
-    private List<Seance> seances = new ArrayList<Seance>();
-    	
+    private ResponseBuilder res;
+    
     public SeanceResource() {
         
     }
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
-    public Collection<Seance> list() {
-        return seances;
+    public Response list() {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	DaoSeance seances = new DaoSeance(em);
+    	
+    	res = Response.ok(seances.findAll());
+    	res.header("Access-Control-Allow-Origin", "*");
+    	res.header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+    	res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+    	
+    	em.close();
+        return res.build();
     }
     
     @GET @Path("search/{id}")
     @Produces({ MediaType.APPLICATION_JSON })
-    public Seance findById(@PathParam("id") String arg0) {
-        return seances.get(Integer.parseInt(arg0));
+    public Response findById(@PathParam("id") String arg0) {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	DaoSeance seances = new DaoSeance(em);
+    	
+    	res = Response.ok(seances.find(Integer.parseInt(arg0)));
+    	res.header("Access-Control-Allow-Origin", "*");
+    	res.header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+    	res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+    	
+    	em.close();
+        return res.build();
     }
 
     @DELETE @Path("delete/{id}")
     @Produces({ MediaType.APPLICATION_JSON })
-    public Seance deleteById(@PathParam("id") String arg0) {
-        return seances.remove(Integer.parseInt(arg0));
+    public Response deleteById(@PathParam("id") String arg0) {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	DaoSeance seances = new DaoSeance(em);
+    	
+    	res = Response.ok(seances.findAll());
+    	res.header("Access-Control-Allow-Origin", "*");
+    	res.header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+    	res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+    	
+    	em.close();
+        return res.build();
     }
     
     @PUT @Path("update/{id}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Seance updateById(@PathParam("id") String arg0, Seance u) {
-    	return seances.set(Integer.parseInt(arg0), u);
+    public Response updateById(@PathParam("id") String arg0, Seance u) {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	DaoSeance seances = new DaoSeance(em);
+    	
+    	res = Response.ok(seances.update(u));
+    	res.header("Access-Control-Allow-Origin", "*");
+    	res.header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+    	res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+    	
+    	em.close();
+        return res.build();
     }
     
     @PUT @Path("add")
     @Produces({MediaType.APPLICATION_JSON})
-    public boolean add(Seance u) {
-    	return seances.add(u);
+    public Response add(Seance u) {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	DaoSeance seances = new DaoSeance(em);
+    	
+    	res = Response.ok(seances.insert(u));
+    	res.header("Access-Control-Allow-Origin", "*");
+    	res.header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS");
+    	res.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+    	
+    	em.close();
+        return res.build();
+    }
+    
+    
+    @OPTIONS
+    @Path("add")
+    public Response getOptionsAdd()
+    {
+	    return Response.ok()
+	    .header("Access-Control-Allow-Origin", "*")
+	    .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
+	    .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").build();
+    }
+    
+    @OPTIONS
+    @Path("delete/{id}")
+    public Response getOptionsDelete()
+    {
+	    return Response.ok()
+	    .header("Access-Control-Allow-Origin", "*")
+	    .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
+	    .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").build();
+    }
+    
+    @OPTIONS
+    @Path("update/{id}")
+    public Response getOptionsUpdate()
+    {
+	    return Response.ok()
+	    .header("Access-Control-Allow-Origin", "*")
+	    .header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
+	    .header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With").build();
     }
 }
